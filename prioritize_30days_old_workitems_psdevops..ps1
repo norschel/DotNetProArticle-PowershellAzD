c4945c2f065489e15e4f18554465eb5a7158c2d6
Nico Orschel <norschel@users.noreply.github.com>
2022-12-12 22:50:17 +0100

    Update prioritize_30days_old_workitems_psdevops..ps1

--- prioritize_30days_old_workitems_psdevops..ps1
+++ prioritize_30days_old_workitems_psdevops..ps1
@@ -8,31 +8,30 @@ param(
     [securestring]$Pat
 )
 # Installation PowerShell Module
-Write-Host "Installing and update latest VSTeam cmdlets"
-Install-Module -Name VSTeam -Repository PSGallery -Scope CurrentUser
-Update-Module -Name VSTeam
-Import-Module -Name VSTeam
-Write-Host "Installed latest version of VSTeam"
+Write-Host "Installing and update latest PSDevOps cmdlets"
+Install-Module -Name PSDevOps -Scope CurrentUser
+Update-Module -Name PSDevOps
+Import-Module -Name PSDevOps
+Write-Host "Installed latest version of PSDevOps"
 
 # Verbindung herstellen
+$apiVersion = "6.0"
 $patPlainText = ConvertFrom-SecureString $Pat -AsPlainText
-Set-VSTeamAccount -Account $Organization -PersonalAccessToken $patPlainText
-Set-VSTeamDefaultProject $Project
+Connect-ADO -Organization $Organization -PersonalAccessToken $patPlainText
 
-# Work Items abfragen
+# Work Items abfragen 
 $query = "Select [System.ID],[System.Title],[Microsoft.VSTS.Common.Priority] from WorkItems where [Changed Date] >= @StartofDay('-30d')"
-$workitems = Get-VSTeamWiql -ProjectName $Project -Query "$query"
+$workitems = Get-ADOWorkItem -Project $Project -Query "$query" -ApiVersion $apiVersion -Fields Microsoft.VSTS.Common.Priority,System.Title
 
-foreach ($workItemID in $workitems.WorkItemIDs) {
-    $workitem = Get-VSTeamWorkItem -Id $workItemID -Fields Microsoft.VSTS.Common.Priority,System.Title
-    write-host "Updating work item $($workItem.ID) - $($workitem.fields."System.Title")"
-    $oldPrio = $workItem.fields."Microsoft.VSTS.Common.Priority";
-    $newPrio = 1
+foreach ($workItem in $workitems) {
+    write-host "Updating work item $($workItem.ID) - $($workitem."System.Title")"
+    $oldValue = $workItem."Microsoft.VSTS.Common.Priority";
+    $newValue = 1;
     Write-Host "Old priority: $($oldPrio)"
     Write-Host "New priority: $($newPrio)"
-
+    
     # Work Item bearbeiten
-    $additionalFields = @{"System.Tags"= "UpdatedByPrioritizeScript"; "Microsoft.VSTS.Common.Priority" = $newPrio}
-    Update-VSTeamWorkItem -ID $workItem.ID -AdditionalFields $additionalFields
+    Set-ADOWorkItem -Project $teamProjectName -ID $workItem.ID -ApiVersion $apiVersion -Tag 'UpdatedByPrioritizeScript' -InputObject @{
+        "Microsoft.VSTS.Common.Priority" = $newPrio;}
     Write-Host "Update was successful"
 }
